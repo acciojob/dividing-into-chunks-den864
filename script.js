d04494bf0659b96ab42128c22d723d66c528805b
Dhaval Birade <69907022+den864@users.noreply.github.com>
2025-08-22 22:54:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+const divide = (nums, k) => {
+    const res=[];
 
-const divide = (arr, n) => {
-  // Write your code here
+    let sum=0;
+    let start=0;
+    for(let i=0;i<nums.length;i++){
+        sum+=nums[i];
+
+        if(sum>k){
+            res.push(...nums.slice(start,i));
+            sum=nums[i];
+            start=i;
+        }
+    }
+    if(start<nums.length) res.push(...nums.slice(start));
+
+    return res;
 };
+
 
 const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
